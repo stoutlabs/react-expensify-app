9c31396b35c2818743d5daf2a036d97ad6761c25
Daniel <daniel@stoutlabs.com>
2017-10-11 01:23:15 -0400

    Add delete functionality to firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,14 @@
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import { setExpenses, startAddExpense, addExpense, editExpense, removeExpense, startSetExpenses } from "../../actions/expenses";
+import {
+  setExpenses,
+  startAddExpense,
+  addExpense,
+  editExpense,
+  removeExpense,
+  startSetExpenses,
+  startRemoveExpense
+} from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
 
@@ -23,6 +31,28 @@ test("should setup remove expense action object", () => {
     type: "REMOVE_EXPENSE",
     id: "123abc"
   });
+});
+
+test("should remove expense from firebase", done => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+  store
+    .dispatch(startRemoveExpense({ id }))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "REMOVE_EXPENSE",
+        id
+      });
+      return database.ref(`expenses/${id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val()).toBeFalsy();
+      done();
+    })
+    .catch(e => {
+      console.log("error removing expense from firebase", e);
+    });
 });
 
 test("should setup edit expense action object", () => {

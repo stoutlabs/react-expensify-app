0ae2b314872d89d1dbe9189724f3254819af7389
Daniel <daniel@stoutlabs.com>
2017-10-16 20:03:26 -0400

    Add Expensify styles

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -17,8 +17,17 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
-        <button onClick={this.onRemove}>Remove</button>
+        <div class="page-header">
+          <div className="content-container">
+            <h1>Edit Expense</h1>
+          </div>
+        </div>
+        <div class="content-container">
+          <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+          <button className="button button--secondary" onClick={this.onRemove}>
+            Remove Expense
+          </button>
+        </div>
       </div>
     );
   }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,14 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <h1>Login</h1>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify</h1>
+      <p>Track your expenses from anywhere!</p>
+      <button className="button" onClick={startLogin}>
+        Login with Google
+      </button>
+    </div>
   </div>
 );
 
